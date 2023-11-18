fdb4487654009c01520634ef49337c6acd6a05d8
Samhernesp <samhernesp@gmail.com>
2023-11-18 10:48:58 -0500

    Add views working

--- clients/urls.py
+++ clients/urls.py
@@ -3,5 +3,7 @@ from clients import views
 from clients.views import AditionalDataView
 
 urlpatterns = [
-      path('aditionalData/', views.AditionalDataView.as_view(), name='aditionalData'),
+    path('aditionalData/', views.AditionalDataView.as_view(), name='aditionalData'),
+    path('registerOrder/', views.RegisterOrderView.as_view(), name='registerOrder'),
+    path('orderDetail/', views.OrderDetailView.as_view(), name='orderDetail'),
 ]
--- clients/views.py
+++ clients/views.py
@@ -1,6 +1,6 @@
 from django.views import View
 from django.shortcuts import render, redirect
-from .forms import ClienteDatosAdicionalesForm
+from .forms import ClienteDatosAdicionalesForm, OrderForm, OrderDetailForm
 
 
 class AditionalDataView(View):
@@ -19,11 +19,11 @@ class AditionalDataView(View):
 
 class RegisterOrderView(View):
     form_class = OrderForm
-    template_name = 'register-order.html'
+    
 
     def get(self, request):
         form = self.form_class()
-        return render(request, self.template_name, {'form': form})
+        return render(request, 'registerOrder.html', {'form': form})
 
     def post(self, request):
         form = self.form_class(request.POST)
@@ -34,41 +34,10 @@ class RegisterOrderView(View):
 
 class OrderDetailView(View):
     form_class = OrderDetailForm
-    template_name = 'register-order-detail.html'
-
+    
     def get(self, request):
         form = self.form_class()
-        return render(request, 'aditionalDataRegister.html', {'form': form})
-
-    def post(self, request):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('url_a_la_siguiente_vista')
-        return render(request, self.template_name, {'form': form})
-
-class RegisterOrderView(View):
-    form_class = OrderForm
-    template_name = 'register-order.html'
-
-    def get(self, request):
-        form = self.form_class()
-        return render(request, self.template_name, {'form': form})
-
-    def post(self, request):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('url_a_la_siguiente_vista')
-        return render(request, self.template_name, {'form': form})
-
-class OrderDetailView(View):
-    form_class = OrderDetailForm
-    template_name = 'register-order-detail.html'
-
-    def get(self, request):
-        form = self.form_class()
-        return render(request, self.template_name, {'form': form})
+        return render(request, 'registerOrderDetail.html', {'form': form})
 
     def post(self, request):
         form = self.form_class(request.POST)
